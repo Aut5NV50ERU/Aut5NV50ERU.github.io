6ae78aca22d176993f0b88692c32f139b8e73790
Aut5NV50ERU <152940771+Aut5NV50ERU@users.noreply.github.com>
2023-12-04 21:06:14 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 /*  Initializa Firebase con la configuración del proyecto. Revisa la
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
-    });
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXmgqNDj7hxs2DsOB3pE-CYWzm5puMgo",
+  authDomain: "aut5nv50eru-e1afc.firebaseapp.com",
+  projectId: "aut5nv50eru-e1afc",
+  storageBucket: "aut5nv50eru-e1afc.appspot.com",
+  messagingSenderId: "54350862370",
+  appId: "1:54350862370:web:3a764a3e9a96f25212d27b",
+  measurementId: "G-C5DNPRJ3TB"
+};
