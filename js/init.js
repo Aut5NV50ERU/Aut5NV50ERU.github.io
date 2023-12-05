0ec59287e5cc89b5b924ab3f3b1a915b3787a00e
Aut5NV50ERU <152940771+Aut5NV50ERU@users.noreply.github.com>
2023-12-04 21:17:14 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,6 +1,11 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCBXmgqNDj7hxs2DsOB3pE-CYWzm5puMgo",
   authDomain: "aut5nv50eru-e1afc.firebaseapp.com",
@@ -10,3 +15,7 @@ const firebaseConfig = {
   appId: "1:54350862370:web:3a764a3e9a96f25212d27b",
   measurementId: "G-C5DNPRJ3TB"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
