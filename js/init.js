98f3b3a5fb706035b0566ac8dd12cda98892acaf
Aut5NV50ERU <152940771+Aut5NV50ERU@users.noreply.github.com>
2023-12-18 23:22:12 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,13 +1,7 @@
-/*  Inicializa Firebase con la
- * configuración del proyecto.
- * Revisa la configuración en tu
- * servidor de Firebase y cópiala
- * aquí sustituyendo los
- * asteriscos. Los campos deben
- * quedar igual que en tu
- * servidor. */
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
 // @ts-ignore
-firebase.initializeApp({
+ const firebaseConfig = {
     apiKey: "AIzaSyD6rM2P17WE6ZyULFjbRgeNNJNIY75Ksjg",
     authDomain: "nube5nv50eru.firebaseapp.com",
     projectId: "nube5nv50eru",
@@ -15,4 +9,4 @@ firebase.initializeApp({
     messagingSenderId: "998088572699",
     appId: "1:998088572699:web:d3dc5b9d126511fe8eddbe",
     measurementId: "G-H0F1DDN6D8"
-});
+  };
